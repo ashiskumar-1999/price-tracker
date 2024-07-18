36f8315265bb97806b61866f9734fcbe058b39f6
Ashis Kumar <ashishsahoo389@gmail.com>
2024-07-18 21:15:05 +0530

    Created custom hook useFetch for fetching the data.

---
+++ src/utils/useFetch.ts
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+type FetchState<T> = {
+  data: any | null;
+  loading: boolean;
+  error: Error | null;
+};
+
+function useFetch<T>(url: string): FetchState<T> {
+  const [state, setState] = useState<FetchState<T>>({
+    data: null,
+    loading: true,
+    error: null,
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+        const data: T = await response.json();
+        setState({ data, loading: false, error: null });
+      } catch (error) {
+        setState({ data: null, loading: false, error: error as Error });
+      }
+    };
+    fetchData();
+  }, [url]);
+  return state;
+}
+export default useFetch;
